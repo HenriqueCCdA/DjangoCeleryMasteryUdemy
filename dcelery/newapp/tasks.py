f976581551995bebf174942c9a4dd07aa480d6be
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-27 21:06:00 -0300

    m

--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,32 +1,32 @@
-from celery import shared_task
-import time
+# from celery import shared_task
+# import time
 
 
-@shared_task
-def task1():
-    return "3"
+# @shared_task
+# def task1():
+#     return "3"
 
 
-@shared_task
-def task2():
-    return "4"
+# @shared_task
+# def task2():
+#     return "4"
 
-@shared_task
-def tp1(queue='celery'):
-    time.sleep(3)
-    return
+# @shared_task
+# def tp1(queue='celery'):
+#     time.sleep(3)
+#     return
 
-@shared_task
-def tp2(queue='celery:1'):
-    time.sleep(3)
-    return
+# @shared_task
+# def tp2(queue='celery:1'):
+#     time.sleep(3)
+#     return
 
-@shared_task
-def tp3(queue='celery:2'):
-    time.sleep(3)
-    return
+# @shared_task
+# def tp3(queue='celery:2'):
+#     time.sleep(3)
+#     return
 
-@shared_task
-def tp4(queue='celery:3'):
-    time.sleep(3)
-    return
+# @shared_task
+# def tp4(queue='celery:3'):
+#     time.sleep(3)
+#     return
